e4ce07b54d04d169270115f20f6db432721bfe3f
Luis Carlos <luis.carlos@jornadatecnologia.com>
2021-11-16 14:56:43 -0300

    feat:install and configuration React Native.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    root: true,
+    extends: ['@react-native-community'],
+    plugins: ['prettier'],
+    rules: {
+        '@typescript-eslint/semi': 'off',
+        'no-unexpected-multiline': 'error',
+    },
 };
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { View } from 'react-native';
+
+const App: React.FC = () => {
+    return (
+        <NavigationContainer>
+            <View />
+        </NavigationContainer>
+    );
+};
+
+export default App;
